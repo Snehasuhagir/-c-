70486c62a27982619f30308202be26751d3238bd
sneha suhagir <127730605+Snehasuhagir@users.noreply.github.com>
2023-04-01 23:48:52 +0530

    Add files via upload

---
+++ sneha.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    string str1(1, 's');
+    string str2(1, 'n');
+    string str3(1, 'e');
+    string str4(1, 'h');
+    string str5(1, 'a');
+    cout<<str1<<str2<<str3<<str4<<str5;
+    return 0;
+
+}
---
+++ str1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    string str1(1 , 's');
+    cout<<str1<<endl;
+    string str2(2 , 'n');
+    cout<<str2<<endl;
+    return 0;
+}
