e7193ba239dc755b6af559a57ad84589acab6110
sneha suhagir <127730605+Snehasuhagir@users.noreply.github.com>
2023-04-02 23:07:11 +0530

    Add files via upload

---
+++ ietd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    string str1(1, 'I');
+    string str2(1, 'E');
+    string str3(1, 'T');
+    string str4(1, '-');
+    string str5(1, 'D');
+    string str6(1, 'A');
+    string str7(1, 'V');
+    string str8(1, 'V');
+    cout<<str1<<str2<<str3<<str4<<str5<<str6<<str7<<str8;
+    return 0;
+}
