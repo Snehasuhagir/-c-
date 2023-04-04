bb880a0e3e0777b2889203f71d9235c843bd5f91
sneha suhagir <127730605+Snehasuhagir@users.noreply.github.com>
2023-04-04 22:49:43 +0530

    Add files via upload

---
+++ btech.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    string str1(1, 'B');
+    string str2(1, 'T');
+    string str3(1, 'e');
+    string str4(1, 'c');
+    string str5(1, 'h');
+    cout<<str1<<str2<<str3<<str4<<str5<<endl;
+    return 0;
+}
