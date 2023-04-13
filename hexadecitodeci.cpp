1e20378078e0732f9214cde8f167fe071de617b7
sneha suhagir <127730605+Snehasuhagir@users.noreply.github.com>
2023-04-13 18:18:47 +0530

    Add files via upload

--- hexadecitodeci.cpp
+++ hexadecitodeci.cpp
@@ -1,28 +1,6 @@
 #include<iostream>
 #include<string>
 using namespace std;
-int binarytodecimal(int n){
-    int ans=0;
-    int x=1;
-    while(n>0){
-        int y= n%10;
-        ans += x*y;
-        x *= 2;
-        n /= 10;
-    }
-    return ans;
-}
-int octaltodecimal(int n){
-     int ans=0;
-    int x=1;
-    while(n>0){
-        int y= n%10;
-        ans += x*y;
-        x *= 8;
-        n /= 10;
-    }
-    return ans;
-}
 int hexadecimaltodecimal(string n){
     int ans=0;
     int x=1;
